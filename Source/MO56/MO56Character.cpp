7317ff7e989fcb104c9cb8ad9ed8b593e342e315
Wesley Weaver <penumbraproductionsllc@gmail.com>
2025-10-28 21:54:50 -0700

    Add HUD widget containers for core UI elements

--- Source/MO56/MO56Character.cpp
+++ Source/MO56/MO56Character.cpp
@@ -19,6 +19,7 @@
 #include "DrawDebugHelpers.h"
 
 #include "InventoryComponent.h" // from MOInventory
+#include "UI/HUDWidget.h"
 
 AMO56Character::AMO56Character()
 {
@@ -58,6 +59,22 @@ AMO56Character::AMO56Character()
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 
 	Inventory = CreateDefaultSubobject<UInventoryComponent>(TEXT("Inventory"));
+}
+
+
+void AMO56Character::BeginPlay()
+{
+        Super::BeginPlay();
+
+        if (HUDWidgetClass)
+        {
+                HUDWidgetInstance = CreateWidget<UHUDWidget>(GetWorld(), HUDWidgetClass);
+
+                if (HUDWidgetInstance)
+                {
+                        HUDWidgetInstance->AddToViewport();
+                }
+        }
 }
 
 
@@ -142,28 +159,28 @@ void AMO56Character::OnInteract(const FInputActionValue& /*Value*/)
 	// bHit = GetWorld()->SweepSingleByObjectType(Hit, ViewLoc, End, FQuat::Identity, ObjParams, Shape, Params);
 	// DrawDebugSphere(GetWorld(), End, Radius, 12, FColor::Cyan, false, 2.f, 0, 1.f);
 
-	DrawDebugLine(GetWorld(), ViewLoc, End, bHit ? FColor::Green : FColor::Red, false, 2.f, 0, 1.f);
+        // DrawDebugLine(GetWorld(), ViewLoc, End, bHit ? FColor::Green : FColor::Red, false, 2.f, 0, 1.f);
 
-	if (!bHit)
-	{
-		UE_LOG(LogMO56, Log, TEXT("Interact trace MISS  Start:%s  End:%s"),
-			*ViewLoc.ToString(), *End.ToString());
-		return;
-	}
+        if (!bHit)
+        {
+                // UE_LOG(LogMO56, Log, TEXT("Interact trace MISS  Start:%s  End:%s"),
+                //       *ViewLoc.ToString(), *End.ToString());
+                return;
+        }
 
-	AActor* HitActor = Hit.GetActor();
-	UE_LOG(LogMO56, Log, TEXT("Interact trace HIT %s at %s"),
-		*GetNameSafe(HitActor), *Hit.ImpactPoint.ToString());
+        AActor* HitActor = Hit.GetActor();
+        // UE_LOG(LogMO56, Log, TEXT("Interact trace HIT %s at %s"),
+        //       *GetNameSafe(HitActor), *Hit.ImpactPoint.ToString());
 
-	if (HitActor && HitActor->GetClass()->ImplementsInterface(UInteractable::StaticClass()))
-	{
-		IInteractable::Execute_Interact(HitActor, this);
-		UE_LOG(LogMO56, Log, TEXT("IInteractable::Interact executed on %s"), *GetNameSafe(HitActor));
-	}
-	else
-	{
-		UE_LOG(LogMO56, Log, TEXT("Hit actor does not implement Interactable"));
-	}
+        if (HitActor && HitActor->GetClass()->ImplementsInterface(UInteractable::StaticClass()))
+        {
+                IInteractable::Execute_Interact(HitActor, this);
+                // UE_LOG(LogMO56, Log, TEXT("IInteractable::Interact executed on %s"), *GetNameSafe(HitActor));
+        }
+        else
+        {
+                // UE_LOG(LogMO56, Log, TEXT("Hit actor does not implement Interactable"));
+        }
 }
 
 void AMO56Character::DoMove(float Right, float Forward)
---
+++ Source/MO56/UI/HUDWidget.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "HUDWidget.generated.h"
+
+class UPanelWidget;
+class UWidget;
+
+/**
+ * HUD widget containing named slots for core gameplay UI elements.
+ */
+UCLASS()
+class MO56_API UHUDWidget : public UUserWidget
+{
+        GENERATED_BODY()
+
+public:
+        /** Container for character status UI elements (health, stamina, etc.) */
+        UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+        TObjectPtr<UPanelWidget> CharacterStatusContainer;
+
+        /** Container for the crosshair UI element */
+        UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+        TObjectPtr<UPanelWidget> CrosshairContainer;
+
+        /** Container for the left inventory panel */
+        UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+        TObjectPtr<UPanelWidget> LeftInventoryContainer;
+
+        /** Container for the right inventory panel */
+        UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+        TObjectPtr<UPanelWidget> RightInventoryContainer;
+
+        /** Container for the mini map widget */
+        UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+        TObjectPtr<UPanelWidget> MiniMapContainer;
+
+public:
+        /** Adds the given widget to the character status container, if available */
+        UFUNCTION(BlueprintCallable, Category = "HUD")
+        void AddCharacterStatusWidget(UWidget* Widget);
+
+        /** Sets the crosshair widget, replacing any existing child */
+        UFUNCTION(BlueprintCallable, Category = "HUD")
+        void SetCrosshairWidget(UWidget* Widget);
+
+        /** Adds a widget to the left inventory container */
+        UFUNCTION(BlueprintCallable, Category = "HUD")
+        void AddLeftInventoryWidget(UWidget* Widget);
+
+        /** Adds a widget to the right inventory container */
+        UFUNCTION(BlueprintCallable, Category = "HUD")
+        void AddRightInventoryWidget(UWidget* Widget);
+
+        /** Sets the mini map widget */
+        UFUNCTION(BlueprintCallable, Category = "HUD")
+        void SetMiniMapWidget(UWidget* Widget);
+
+private:
+        void AddWidgetToContainer(UPanelWidget* Container, UWidget* Widget, bool bClearChildren);
+};
+
