4f87c5af4f905da22d1602dc881db21c214aaeb9
penpro <penumbraproductionsllc@gmail.com>
2025-10-29 09:56:31 -0700

    Update MO56.Build.cs

--- Source/MO56/MO56.Build.cs
+++ Source/MO56/MO56.Build.cs
@@ -27,20 +27,7 @@ public class MO56 : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		PublicIncludePaths.AddRange(new string[] {
-			"MO56",
-			"MO56/Variant_Platforming",
-			"MO56/Variant_Platforming/Animation",
-			"MO56/Variant_Combat",
-			"MO56/Variant_Combat/AI",
-			"MO56/Variant_Combat/Animation",
-			"MO56/Variant_Combat/Gameplay",
-			"MO56/Variant_Combat/Interfaces",
-			"MO56/Variant_Combat/UI",
-			"MO56/Variant_SideScrolling",
-			"MO56/Variant_SideScrolling/AI",
-			"MO56/Variant_SideScrolling/Gameplay",
-			"MO56/Variant_SideScrolling/Interfaces",
-			"MO56/Variant_SideScrolling/UI"
+			"MO56"
 		});
 
 		// Uncomment if you are using Slate UI
