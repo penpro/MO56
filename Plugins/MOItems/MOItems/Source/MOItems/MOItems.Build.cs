8bed71c62e094ffe1a644333c14d855f91282440
penpro <penumbraproductionsllc@gmail.com>
2025-10-29 17:06:28 -0700

    compiler troubleshooting

--- Plugins/MOItems/MOItems/Source/MOItems/MOItems.Build.cs
+++ Plugins/MOItems/MOItems/Source/MOItems/MOItems.Build.cs
@@ -22,15 +22,15 @@ public class MOItems : ModuleRules
 			);
 			
 		
-                PublicDependencyModuleNames.AddRange(
-                        new string[]
-                        {
-                                "Core",
-                                "CoreUObject",
-                                "Engine",
-                                // ... add other public dependencies that you statically link with here ...
-                        }
-                        );
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+				{
+                        "Core",
+                        "CoreUObject",
+                        "Engine",
+				        // ... add other public dependencies that you statically link with here ...
+			        }
+                );
 			
 		
 		PrivateDependencyModuleNames.AddRange(
