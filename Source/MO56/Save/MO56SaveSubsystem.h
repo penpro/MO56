593558bd2c9fbd0c6396fd3d453b58b49f2ca57a
penpro <penumbraproductionsllc@gmail.com>
2025-11-02 10:57:06 -0800

    main menu work

--- Source/MO56/Save/MO56SaveSubsystem.h
+++ Source/MO56/Save/MO56SaveSubsystem.h
@@ -224,7 +224,7 @@ private:
         TMap<FGuid, TWeakObjectPtr<AMO56Character>> RegisteredCharacters;
 
         TMap<UWorld*, FDelegateHandle> WorldSpawnHandles;
-        FDelegateHandle PostWorldInitHandle;
+
         FDelegateHandle WorldCleanupHandle;
         FDelegateHandle PostLoadMapHandle;
 
@@ -237,7 +237,7 @@ private:
         void HandlePostWorldInit(UWorld* World, const UWorld::InitializationValues IVS);
         void HandleWorldCleanup(UWorld* World, bool bSessionEnded, bool bCleanupResources);
         void HandleActorSpawned(AActor* Actor);
-        void HandlePostLoadMapWithWorld(UWorld* World);
+
 
         UFUNCTION()
         void HandlePickupSettled(AItemPickup* Pickup);
